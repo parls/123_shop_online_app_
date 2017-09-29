62a01020b986f21922ef260e9194d5d8d2d12217
Palesa Mathepe <parlsmathepe@gmail.com>
2017-09-29 12:10:20 +0200

    Initial commit and project setup complete

---
+++ shoppingbackend/src/main/java/net/parls/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.parls.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
