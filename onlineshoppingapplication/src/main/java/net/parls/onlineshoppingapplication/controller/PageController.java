039505bd3b9b1a4fad7621c46c0a16e033a02a65
Parls <parlsmathepe@gmail.com>
2017-10-03 14:33:12 +0200

    Static resources successfully loaded

--- onlineshoppingapplication/src/main/java/net/parls/onlineshoppingapplication/controller/PageController.java
+++ onlineshoppingapplication/src/main/java/net/parls/onlineshoppingapplication/controller/PageController.java
@@ -1,7 +1,9 @@
 package net.parls.onlineshoppingapplication.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -12,6 +14,17 @@ public class PageController {
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
 		mv.addObject("greeting","Well done. You've done it!!!");
+		return mv;
+	}
+	
+	@RequestMapping(value="/test/{greeting}")
+	public ModelAndView test(@PathVariable("greeting")String greeting){
+		
+		if(greeting==null) {
+			greeting = "Hello";
+		}
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting",greeting);
 		return mv;
 	}
 	
